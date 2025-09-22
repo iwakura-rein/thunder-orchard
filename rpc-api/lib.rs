LayerTwo-Labs/thunder-rust

Optimize p2p / net task, add forget_peer RPC

rpc-api/lib.rs
@@ -55,6 +55,17 @@ pub trait Rpc {
         address: Address,
     ) -> RpcResult<String>;
 
+    /// Delete peer from known_peers DB.
+    /// Connections to the peer are not terminated.
+    #[method(name = "forget_peer")]
+    async fn forget_peer(
+        &self,
+        #[open_api_method_arg(schema(
+            PartialSchema = "thunder_schema::SocketAddr"
+        ))]
+        addr: SocketAddr,
+    ) -> RpcResult<()>;
+
     /// Generate a mnemonic seed phrase
     #[method(name = "generate_mnemonic")]
     async fn generate_mnemonic(&self) -> RpcResult<String>;

iwakura-rein/thunder-orchard

rpc-api/lib.rs
@@ -49,6 +49,17 @@ pub trait Rpc {
         fee_sats: u64,
     ) -> RpcResult<bitcoin::Txid>;
 
+    /// Delete peer from known_peers DB.
+    /// Connections to the peer are not terminated.
+    #[method(name = "forget_peer")]
+    async fn forget_peer(
+        &self,
+        #[open_api_method_arg(schema(
+            PartialSchema = "thunder_orchard_schema::SocketAddr"
+        ))]
+        addr: SocketAddr,
+    ) -> RpcResult<()>;
+
     /// Format a deposit address
     #[method(name = "format_deposit_address")]
     async fn format_deposit_address(