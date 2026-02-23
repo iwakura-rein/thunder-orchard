LayerTwo-Labs/thunder-rust

P2P: always send/expect network magic bytes

lib/net/peer/message.rs
@@ -7,8 +7,28 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     net::peer::{PeerState, PeerStateId},
-    types::{AuthorizedTransaction, BlockHash, Body, Header, Tip, Txid},
+    types::{
+        AuthorizedTransaction, BlockHash, Body, Header, Network, Tip, Txid,
+    },
 };
+
+pub const MAGIC_BYTES_LEN: usize = 4;
+
+pub type MagicBytes = [u8; MAGIC_BYTES_LEN];
+
+pub const fn magic_bytes(network: Network) -> MagicBytes {
+    // First 4 bytes are the US-TTY (LSB Right) Baudot–Murray code for "THNDR".
+    // Rightmost bits of the 4th byte is the network identifier.
+    let b0 = 0b1000_0101;
+    let b1 = 0b0001_1000;
+    let b2 = 0b1001_0101;
+    let mut b3 = 0b0000_0000;
+    match network {
+        Network::Regtest => (),
+        Network::Signet => b3 |= 0b0000_0001,
+    }
+    [b0, b1, b2, b3]
+}
 
 #[derive(BorshSerialize, Clone, Debug, Deserialize, Serialize)]
 pub struct Heartbeat(pub PeerState);

iwakura-rein/thunder-orchard

lib/net/peer/message.rs
@@ -7,8 +7,28 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     net::peer::{PeerState, PeerStateId},
-    types::{AuthorizedTransaction, BlockHash, Body, Header, Tip, Txid},
+    types::{
+        AuthorizedTransaction, BlockHash, Body, Header, Network, Tip, Txid,
+    },
 };
+
+pub const MAGIC_BYTES_LEN: usize = 4;
+
+pub type MagicBytes = [u8; MAGIC_BYTES_LEN];
+
+pub const fn magic_bytes(network: Network) -> MagicBytes {
+    // First 4 bytes are the US-TTY (LSB Right) Baudot–Murray code for "ZSIDE".
+    // Rightmost bits of the 4th byte is the network identifier.
+    let b0 = 0b1000_1101;
+    let b1 = 0b0001_1001;
+    let b2 = 0b0010_1000;
+    let mut b3 = 0b0000_0000;
+    match network {
+        Network::Regtest => (),
+        Network::Signet => b3 |= 0b0000_0001,
+    }
+    [b0, b1, b2, b3]
+}
 
 #[derive(BorshSerialize, Clone, Debug, Deserialize, Serialize)]
 pub struct Heartbeat(pub PeerState);