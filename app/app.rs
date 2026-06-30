LayerTwo-Labs/thunder-rust

Fix GUI layout squishing and small default window size

app/app.rs
@@ -287,30 +287,29 @@ impl App {
         Ok(())
     }
 
-    pub fn get_new_main_address(
+    pub async fn get_new_main_address_async(
         &self,
     ) -> Result<bitcoin::Address<bitcoin::address::NetworkChecked>, Error> {
         let Some(miner) = self.miner.as_ref() else {
             return Err(Error::NoCusfMainchainWalletClient);
         };
-        let address = self.runtime.block_on({
-            let miner = miner.clone();
-            async move {
-                let mut miner_write = miner.write().await;
-                let cusf_mainchain = &mut miner_write.cusf_mainchain;
-                let mainchain_info = cusf_mainchain.get_chain_info().await?;
-                let cusf_mainchain_wallet =
-                    &mut miner_write.cusf_mainchain_wallet;
-                let res = cusf_mainchain_wallet
-                    .create_new_address()
-                    .await?
-                    .require_network(mainchain_info.network)
-                    .unwrap();
-                drop(miner_write);
-                Result::<_, Error>::Ok(res)
-            }
-        })?;
-        Ok(address)
+        let mut miner_write = miner.write().await;
+        let cusf_mainchain = &mut miner_write.cusf_mainchain;
+        let mainchain_info = cusf_mainchain.get_chain_info().await?;
+        let cusf_mainchain_wallet = &mut miner_write.cusf_mainchain_wallet;
+        let res = cusf_mainchain_wallet
+            .create_new_address()
+            .await?
+            .require_network(mainchain_info.network)
+            .unwrap();
+        drop(miner_write);
+        Ok(res)
+    }
+
+    pub fn get_new_main_address(
+        &self,
+    ) -> Result<bitcoin::Address<bitcoin::address::NetworkChecked>, Error> {
+        self.runtime.block_on(self.get_new_main_address_async())
     }
 
     const EMPTY_BLOCK_BMM_BRIBE: bitcoin::Amount =
@@ -531,7 +530,7 @@ impl App {
         Ok(())
     }
 
-    pub fn deposit(
+    pub async fn deposit_async(
         &self,
         address: Address,
         amount: bitcoin::Amount,
@@ -540,15 +539,23 @@ impl App {
         let Some(miner) = self.miner.as_ref() else {
             return Err(Error::NoCusfMainchainWalletClient);
         };
-        self.runtime.block_on(async {
-            let mut miner_write = miner.write().await;
-            let txid = miner_write
-                .cusf_mainchain_wallet
-                .create_deposit_tx(address, amount.to_sat(), fee.to_sat())
-                .await?;
-            drop(miner_write);
-            Ok(txid)
-        })
+        let mut miner_write = miner.write().await;
+        let txid = miner_write
+            .cusf_mainchain_wallet
+            .create_deposit_tx(address, amount.to_sat(), fee.to_sat())
+            .await?;
+        drop(miner_write);
+        Ok(txid)
+    }
+
+    pub fn deposit(
+        &self,
+        address: Address,
+        amount: bitcoin::Amount,
+        fee: bitcoin::Amount,
+    ) -> Result<bitcoin::Txid, Error> {
+        self.runtime
+            .block_on(self.deposit_async(address, amount, fee))
     }
 }
 

iwakura-rein/thunder-orchard

app/app.rs
@@ -369,30 +369,29 @@ impl App {
         Ok(())
     }
 
-    pub fn get_new_main_address(
+    pub async fn get_new_main_address_async(
         &self,
     ) -> Result<bitcoin::Address<bitcoin::address::NetworkChecked>, Error> {
         let Some(miner) = self.miner.as_ref() else {
             return Err(Error::NoCusfMainchainWalletClient);
         };
-        let address = self.runtime.block_on({
-            let miner = miner.clone();
-            async move {
-                let mut miner_write = miner.write().await;
-                let cusf_mainchain = &mut miner_write.cusf_mainchain;
-                let mainchain_info = cusf_mainchain.get_chain_info().await?;
-                let cusf_mainchain_wallet =
-                    &mut miner_write.cusf_mainchain_wallet;
-                let res = cusf_mainchain_wallet
-                    .create_new_address()
-                    .await?
-                    .require_network(mainchain_info.network)
-                    .unwrap();
-                drop(miner_write);
-                Result::<_, Error>::Ok(res)
-            }
-        })?;
-        Ok(address)
+        let mut miner_write = miner.write().await;
+        let cusf_mainchain = &mut miner_write.cusf_mainchain;
+        let mainchain_info = cusf_mainchain.get_chain_info().await?;
+        let cusf_mainchain_wallet = &mut miner_write.cusf_mainchain_wallet;
+        let res = cusf_mainchain_wallet
+            .create_new_address()
+            .await?
+            .require_network(mainchain_info.network)
+            .unwrap();
+        drop(miner_write);
+        Ok(res)
+    }
+
+    pub fn get_new_main_address(
+        &self,
+    ) -> Result<bitcoin::Address<bitcoin::address::NetworkChecked>, Error> {
+        self.runtime.block_on(self.get_new_main_address_async())
     }
 
     const EMPTY_BLOCK_BMM_BRIBE: bitcoin::Amount =
@@ -610,7 +609,7 @@ impl App {
         Ok(())
     }
 
-    pub fn deposit(
+    pub async fn deposit_async(
         &self,
         address: TransparentAddress,
         amount: bitcoin::Amount,
@@ -619,15 +618,23 @@ impl App {
         let Some(miner) = self.miner.as_ref() else {
             return Err(Error::NoCusfMainchainWalletClient);
         };
-        self.runtime.block_on(async {
-            let mut miner_write = miner.write().await;
-            let txid = miner_write
-                .cusf_mainchain_wallet
-                .create_deposit_tx(address, amount.to_sat(), fee.to_sat())
-                .await?;
-            drop(miner_write);
-            Ok(txid)
-        })
+        let mut miner_write = miner.write().await;
+        let txid = miner_write
+            .cusf_mainchain_wallet
+            .create_deposit_tx(address, amount.to_sat(), fee.to_sat())
+            .await?;
+        drop(miner_write);
+        Ok(txid)
+    }
+
+    pub fn deposit(
+        &self,
+        address: TransparentAddress,
+        amount: bitcoin::Amount,
+        fee: bitcoin::Amount,
+    ) -> Result<bitcoin::Txid, Error> {
+        self.runtime
+            .block_on(self.deposit_async(address, amount, fee))
     }
 }
 