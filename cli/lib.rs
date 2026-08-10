LayerTwo-Labs/thunder-rust

Update wallet API

cli/lib.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{marker::PhantomData, net::SocketAddr, time::Duration};
 
 use clap::{Parser, Subcommand};
 use http::HeaderMap;
@@ -7,6 +7,20 @@ use jsonrpsee::{core::client::ClientT, http_client::HttpClientBuilder};
 use thunder::types::{Address, Txid};
 use thunder_app_rpc_api::RpcClient;
 use tracing_subscriber::layer::SubscriberExt as _;
+
+struct JsonParser<T>(PhantomData<T>);
+
+impl<T> JsonParser<T> {
+    fn parse(
+        s: &str,
+    ) -> Result<T, serde_path_to_error::Error<serde_json::Error>>
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        let mut deserializer = serde_json::Deserializer::from_str(s);
+        serde_path_to_error::deserialize(&mut deserializer)
+    }
+}
 
 #[derive(Clone, Debug, Subcommand)]
 #[command(arg_required_else_help(true))]
@@ -22,6 +36,26 @@ pub enum Command {
         value_sats: u64,
         #[arg(long)]
         fee_sats: u64,
+    },
+    /// Create a tx that transfers funds to the specified address,
+    /// without signing it
+    CreateTransfer {
+        dest: Address,
+        #[arg(long)]
+        value_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+    },
+    /// Creates a tx that initiates a withdrawal to the specified mainchain
+    /// address, without signing it
+    CreateWithdrawal {
+        mainchain_address: bitcoin::Address<bitcoin::address::NetworkUnchecked>,
+        #[arg(long)]
+        amount_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+        #[arg(long)]
+        mainchain_fee_sats: u64,
     },
     /// Format a deposit address
     FormatDepositAddress { address: Address },
@@ -74,26 +108,23 @@ pub enum Command {
     SetSeedFromMnemonic { mnemonic: String },
     /// Get total sidechain wealth
     SidechainWealth,
+    /// Sign a transaction, and optionally broadcast it.
+    SignTransaction {
+        #[arg(value_parser = JsonParser::<thunder::types::Transaction>::parse)]
+        transaction: thunder::types::Transaction,
+        #[arg(default_value_t = false)]
+        broadcast: bool,
+    },
+    /// Verify and broadcast a transaction
+    SubmitTransaction {
+        #[arg(
+            value_parser =
+                JsonParser::<thunder::types::AuthorizedTransaction>::parse
+        )]
+        transaction: thunder::types::AuthorizedTransaction,
+    },
     /// Stop the node
     Stop,
-    /// Transfer funds to the specified address
-    Transfer {
-        dest: Address,
-        #[arg(long)]
-        value_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-    },
-    /// Initiate a withdrawal to the specified mainchain address
-    Withdraw {
-        mainchain_address: bitcoin::Address<bitcoin::address::NetworkUnchecked>,
-        #[arg(long)]
-        amount_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-        #[arg(long)]
-        mainchain_fee_sats: u64,
-    },
 }
 
 #[derive(Clone, Debug, Parser)]
@@ -138,6 +169,32 @@ where
                 .create_deposit(address, value_sats, fee_sats)
                 .await?;
             format!("{txid}")
+        }
+        Command::CreateTransfer {
+            dest,
+            value_sats,
+            fee_sats,
+        } => {
+            let tx = rpc_client
+                .create_transfer(dest, value_sats, fee_sats)
+                .await?;
+            serde_json::to_string_pretty(&tx)?
+        }
+        Command::CreateWithdrawal {
+            mainchain_address,
+            amount_sats,
+            fee_sats,
+            mainchain_fee_sats,
+        } => {
+            let tx = rpc_client
+                .create_withdrawal(
+                    mainchain_address,
+                    amount_sats,
+                    fee_sats,
+                    mainchain_fee_sats,
+                )
+                .await?;
+            serde_json::to_string_pretty(&tx)?
         }
         Command::FormatDepositAddress { address } => {
             rpc_client.format_deposit_address(address).await?
@@ -223,33 +280,22 @@ where
             let sidechain_wealth = rpc_client.sidechain_wealth_sats().await?;
             format!("{sidechain_wealth}")
         }
+        Command::SignTransaction {
+            transaction,
+            broadcast,
+        } => {
+            let authorized = rpc_client
+                .sign_transaction(transaction, Some(broadcast))
+                .await?;
+            serde_json::to_string_pretty(&authorized)?
+        }
+        Command::SubmitTransaction { transaction } => {
+            let txid = rpc_client.submit_transaction(transaction).await?;
+            format!("{txid}")
+        }
         Command::Stop => {
             let () = rpc_client.stop().await?;
             String::default()
-        }
-        Command::Transfer {
-            dest,
-            value_sats,
-            fee_sats,
-        } => {
-            let txid = rpc_client.transfer(dest, value_sats, fee_sats).await?;
-            format!("{txid}")
-        }
-        Command::Withdraw {
-            mainchain_address,
-            amount_sats,
-            fee_sats,
-            mainchain_fee_sats,
-        } => {
-            let txid = rpc_client
-                .withdraw(
-                    mainchain_address,
-                    amount_sats,
-                    fee_sats,
-                    mainchain_fee_sats,
-                )
-                .await?;
-            format!("{txid}")
         }
     })
 }

lib/node/mod.rs
@@ -245,12 +245,12 @@ where
 
     pub fn submit_transaction(
         &self,
-        transaction: AuthorizedTransaction,
+        transaction: &AuthorizedTransaction,
     ) -> Result<(), Error> {
         {
             let mut rotxn = self.env.write_txn().map_err(EnvError::from)?;
-            self.state.validate_transaction(&rotxn, &transaction)?;
-            self.mempool.put(&mut rotxn, &transaction)?;
+            self.state.validate_transaction(&rotxn, transaction)?;
+            self.mempool.put(&mut rotxn, transaction)?;
             rotxn.commit().map_err(RwTxnError::from)?;
         }
         self.net.push_tx(Default::default(), transaction);

rpc-api/lib.rs
@@ -8,8 +8,8 @@ use serde::{Deserialize, Serialize};
 use thunder::{
     net::Peer,
     types::{
-        Address, BlockHash, MerkleRoot, OutPoint, Output, OutputContent,
-        PointedOutput, Transaction, Txid, WithdrawalBundle,
+        Address, Authorized, BlockHash, MerkleRoot, OutPoint, Output,
+        OutputContent, PointedOutput, Transaction, Txid, WithdrawalBundle,
         schema as thunder_schema,
     },
     wallet::Balance,
@@ -57,6 +57,32 @@ pub trait Rpc {
         value_sats: u64,
         fee_sats: u64,
     ) -> RpcResult<bitcoin::Txid>;
+
+    /// Create a tx that transfers funds to the specified address,
+    /// without signing it
+    #[method(name = "create_transfer")]
+    async fn create_transfer(
+        &self,
+        dest: Address,
+        value_sats: u64,
+        fee_sats: u64,
+    ) -> RpcResult<Transaction>;
+
+    /// Creates a tx that initiates a withdrawal to the specified mainchain
+    /// address, without signing it
+    #[method(name = "create_withdrawal")]
+    async fn create_withdrawal(
+        &self,
+        #[open_api_method_arg(schema(
+            PartialSchema = "thunder::types::schema::BitcoinAddr"
+        ))]
+        mainchain_address: bitcoin::Address<
+            bitcoin::address::NetworkUnchecked,
+        >,
+        amount_sats: u64,
+        fee_sats: u64,
+        mainchain_fee_sats: u64,
+    ) -> RpcResult<Transaction>;
 
     /// Format a deposit address
     #[method(name = "format_deposit_address")]
@@ -183,31 +209,22 @@ pub trait Rpc {
     #[method(name = "sidechain_wealth")]
     async fn sidechain_wealth_sats(&self) -> RpcResult<u64>;
 
+    /// Sign a transaction, and optionally broadcast it.
+    #[method(name = "sign_transaction")]
+    async fn sign_transaction(
+        &self,
+        transaction: Transaction,
+        broadcast: Option<bool>,
+    ) -> RpcResult<Authorized<Transaction>>;
+
+    /// Verify and broadcast a transaction
+    #[method(name = "submit_transaction")]
+    async fn submit_transaction(
+        &self,
+        transaction: Authorized<Transaction>,
+    ) -> RpcResult<Txid>;
+
     /// Stop the node
     #[method(name = "stop")]
     async fn stop(&self);
-
-    /// Transfer funds to the specified address
-    #[method(name = "transfer")]
-    async fn transfer(
-        &self,
-        dest: Address,
-        value_sats: u64,
-        fee_sats: u64,
-    ) -> RpcResult<Txid>;
-
-    /// Initiate a withdrawal to the specified mainchain address
-    #[method(name = "withdraw")]
-    async fn withdraw(
-        &self,
-        #[open_api_method_arg(schema(
-            PartialSchema = "thunder::types::schema::BitcoinAddr"
-        ))]
-        mainchain_address: bitcoin::Address<
-            bitcoin::address::NetworkUnchecked,
-        >,
-        amount_sats: u64,
-        fee_sats: u64,
-        mainchain_fee_sats: u64,
-    ) -> RpcResult<Txid>;
 }

iwakura-rein/thunder-orchard

cli/lib.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{marker::PhantomData, net::SocketAddr, time::Duration};
 
 use clap::{Parser, Subcommand};
 use http::HeaderMap;
@@ -7,6 +7,20 @@ use jsonrpsee::{core::client::ClientT, http_client::HttpClientBuilder};
 use thunder_orchard::types::{ShieldedAddress, TransparentAddress, Txid};
 use thunder_orchard_app_rpc_api::RpcClient;
 use tracing_subscriber::layer::SubscriberExt as _;
+
+struct JsonParser<T>(PhantomData<T>);
+
+impl<T> JsonParser<T> {
+    fn parse(
+        s: &str,
+    ) -> Result<T, serde_path_to_error::Error<serde_json::Error>>
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        let mut deserializer = serde_json::Deserializer::from_str(s);
+        serde_path_to_error::deserialize(&mut deserializer)
+    }
+}
 
 #[derive(Clone, Debug, Subcommand)]
 #[command(arg_required_else_help(true))]
@@ -22,6 +36,48 @@ pub enum Command {
         value_sats: u64,
         #[arg(long)]
         fee_sats: u64,
+    },
+    /// Create a tx that shields transparent funds
+    CreateShield {
+        #[arg(long)]
+        value_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+    },
+    /// Create a tx that transfers shielded funds to the specified address
+    CreateShieldedTransfer {
+        dest: ShieldedAddress,
+        #[arg(long)]
+        value_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+    },
+    /// Create a tx that transfers funds to the specified address
+    /// transparently
+    CreateTransparentTransfer {
+        dest: TransparentAddress,
+        #[arg(long)]
+        value_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+    },
+    /// Create a tx that unshields shielded funds
+    CreateUnshield {
+        #[arg(long)]
+        value_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+    },
+    /// Creates a tx that initiates a withdrawal to the specified mainchain
+    /// address
+    CreateWithdrawal {
+        mainchain_address: bitcoin::Address<bitcoin::address::NetworkUnchecked>,
+        #[arg(long)]
+        amount_sats: u64,
+        #[arg(long)]
+        fee_sats: u64,
+        #[arg(long)]
+        mainchain_fee_sats: u64,
     },
     /// Delete peer from known_peers DB.
     /// Connections to the peer are not terminated.
@@ -82,50 +138,25 @@ pub enum Command {
     RemoveFromMempool { txid: Txid },
     /// Set the wallet seed from a mnemonic seed phrase
     SetSeedFromMnemonic { mnemonic: String },
-    /// Shield transparent funds
-    Shield {
-        #[arg(long)]
-        value_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-    },
-    /// Transfer shielded funds to the specified address
-    ShieldedTransfer {
-        dest: ShieldedAddress,
-        #[arg(long)]
-        value_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-    },
     /// Get total sidechain wealth
     SidechainWealth,
+    /// Sign a transaction, and optionally broadcast it.
+    SignTransaction {
+        #[arg(value_parser = JsonParser::<thunder_orchard::types::Transaction>::parse)]
+        transaction: thunder_orchard::types::Transaction,
+        #[arg(default_value_t = false)]
+        broadcast: bool,
+    },
+    /// Verify and broadcast a transaction
+    SubmitTransaction {
+        #[arg(
+            value_parser =
+                JsonParser::<thunder_orchard::types::AuthorizedTransaction>::parse
+        )]
+        transaction: thunder_orchard::types::AuthorizedTransaction,
+    },
     /// Stop the node
     Stop,
-    /// Transfer transparent funds to the specified address
-    TransparentTransfer {
-        dest: TransparentAddress,
-        #[arg(long)]
-        value_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-    },
-    /// Unshield shielded funds
-    Unshield {
-        #[arg(long)]
-        value_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-    },
-    /// Initiate a withdrawal to the specified mainchain address
-    Withdraw {
-        mainchain_address: bitcoin::Address<bitcoin::address::NetworkUnchecked>,
-        #[arg(long)]
-        amount_sats: u64,
-        #[arg(long)]
-        fee_sats: u64,
-        #[arg(long)]
-        mainchain_fee_sats: u64,
-    },
 }
 
 fn default_rpc_url() -> url::Url {
@@ -176,6 +207,56 @@ where
         } => {
             let txid = rpc_client
                 .create_deposit(address, value_sats, fee_sats)
+                .await?;
+            format!("{txid}")
+        }
+        Command::CreateShield {
+            value_sats,
+            fee_sats,
+        } => {
+            let txid = rpc_client.create_shield(value_sats, fee_sats).await?;
+            format!("{txid}")
+        }
+        Command::CreateShieldedTransfer {
+            dest,
+            value_sats,
+            fee_sats,
+        } => {
+            let txid = rpc_client
+                .create_shielded_transfer(dest, value_sats, fee_sats)
+                .await?;
+            format!("{txid}")
+        }
+        Command::CreateUnshield {
+            value_sats,
+            fee_sats,
+        } => {
+            let txid = rpc_client.create_unshield(value_sats, fee_sats).await?;
+            format!("{txid}")
+        }
+        Command::CreateTransparentTransfer {
+            dest,
+            value_sats,
+            fee_sats,
+        } => {
+            let txid = rpc_client
+                .create_transparent_transfer(dest, value_sats, fee_sats)
+                .await?;
+            format!("{txid}")
+        }
+        Command::CreateWithdrawal {
+            mainchain_address,
+            amount_sats,
+            fee_sats,
+            mainchain_fee_sats,
+        } => {
+            let txid = rpc_client
+                .create_withdrawal(
+                    mainchain_address,
+                    amount_sats,
+                    fee_sats,
+                    mainchain_fee_sats,
+                )
                 .await?;
             format!("{txid}")
         }
@@ -280,63 +361,26 @@ where
             let () = rpc_client.set_seed_from_mnemonic(mnemonic).await?;
             String::default()
         }
-        Command::Shield {
-            value_sats,
-            fee_sats,
-        } => {
-            let txid = rpc_client.shield(value_sats, fee_sats).await?;
-            format!("{txid}")
-        }
-        Command::ShieldedTransfer {
-            dest,
-            value_sats,
-            fee_sats,
-        } => {
-            let txid = rpc_client
-                .shielded_transfer(dest, value_sats, fee_sats)
-                .await?;
-            format!("{txid}")
-        }
         Command::SidechainWealth => {
             let sidechain_wealth = rpc_client.sidechain_wealth_sats().await?;
             format!("{sidechain_wealth}")
         }
+        Command::SignTransaction {
+            transaction,
+            broadcast,
+        } => {
+            let authorized = rpc_client
+                .sign_transaction(transaction, Some(broadcast))
+                .await?;
+            serde_json::to_string_pretty(&authorized)?
+        }
+        Command::SubmitTransaction { transaction } => {
+            let txid = rpc_client.submit_transaction(transaction).await?;
+            format!("{txid}")
+        }
         Command::Stop => {
             let () = rpc_client.stop().await?;
             String::default()
-        }
-        Command::TransparentTransfer {
-            dest,
-            value_sats,
-            fee_sats,
-        } => {
-            let txid = rpc_client
-                .transparent_transfer(dest, value_sats, fee_sats)
-                .await?;
-            format!("{txid}")
-        }
-        Command::Unshield {
-            value_sats,
-            fee_sats,
-        } => {
-            let txid = rpc_client.unshield(value_sats, fee_sats).await?;
-            format!("{txid}")
-        }
-        Command::Withdraw {
-            mainchain_address,
-            amount_sats,
-            fee_sats,
-            mainchain_fee_sats,
-        } => {
-            let txid = rpc_client
-                .withdraw(
-                    mainchain_address,
-                    amount_sats,
-                    fee_sats,
-                    mainchain_fee_sats,
-                )
-                .await?;
-            format!("{txid}")
         }
     })
 }

lib/node/mod.rs
@@ -189,12 +189,12 @@ where
 
     pub fn submit_transaction(
         &self,
-        transaction: AuthorizedTransaction,
+        transaction: &AuthorizedTransaction,
     ) -> Result<(), error::SubmitTransaction> {
         {
             let mut rwtxn = self.env.write_txn()?;
-            self.state.validate_transaction(&rwtxn, &transaction)?;
-            self.mempool.insert(&mut rwtxn, &transaction)?;
+            self.state.validate_transaction(&rwtxn, transaction)?;
+            self.mempool.insert(&mut rwtxn, transaction)?;
             rwtxn.commit()?;
         }
         self.net.push_tx(Default::default(), transaction);

rpc-api/lib.rs
@@ -10,7 +10,7 @@ use thunder_orchard::{
     types::{
         BlockHash, MerkleRoot, OutPoint, Output, OutputContent, PointedOutput,
         ShieldedAddress, SpentOutput, Transaction, TransparentAddress, Txid,
-        WithdrawalBundle, schema as thunder_orchard_schema,
+        WithdrawalBundle, schema as thunder_orchard_schema, transaction,
     },
     wallet::Balance,
 };
@@ -57,6 +57,57 @@ pub trait Rpc {
         value_sats: u64,
         fee_sats: u64,
     ) -> RpcResult<bitcoin::Txid>;
+
+    /// Create a tx that shields transparent funds
+    #[method(name = "create_shield")]
+    async fn create_shield(
+        &self,
+        value_sats: u64,
+        fee_sats: u64,
+    ) -> RpcResult<Txid>;
+
+    /// Create a tx that transfers shielded funds to the specified address
+    #[method(name = "create_shielded_transfer")]
+    async fn create_shielded_transfer(
+        &self,
+        dest: ShieldedAddress,
+        value_sats: u64,
+        fee_sats: u64,
+    ) -> RpcResult<Txid>;
+
+    /// Create a tx that transfers funds to the specified address
+    /// transparently
+    #[method(name = "create_transparent_transfer")]
+    async fn create_transparent_transfer(
+        &self,
+        dest: TransparentAddress,
+        value_sats: u64,
+        fee_sats: u64,
+    ) -> RpcResult<Txid>;
+
+    /// Create a tx that unshields shielded funds
+    #[method(name = "create_unshield")]
+    async fn create_unshield(
+        &self,
+        value_sats: u64,
+        fee_sats: u64,
+    ) -> RpcResult<Txid>;
+
+    /// Creates a tx that initiates a withdrawal to the specified mainchain
+    /// address
+    #[method(name = "create_withdrawal")]
+    async fn create_withdrawal(
+        &self,
+        #[open_api_method_arg(schema(
+            PartialSchema = "thunder_orchard::types::schema::BitcoinAddr"
+        ))]
+        mainchain_address: bitcoin::Address<
+            bitcoin::address::NetworkUnchecked,
+        >,
+        amount_sats: u64,
+        fee_sats: u64,
+        mainchain_fee_sats: u64,
+    ) -> RpcResult<Txid>;
 
     /// Delete peer from known_peers DB.
     /// Connections to the peer are not terminated.
@@ -211,53 +262,26 @@ pub trait Rpc {
     #[method(name = "set_seed_from_mnemonic")]
     async fn set_seed_from_mnemonic(&self, mnemonic: String) -> RpcResult<()>;
 
-    /// Shield transparent funds
-    #[method(name = "shield")]
-    async fn shield(&self, value_sats: u64, fee_sats: u64) -> RpcResult<Txid>;
-
-    /// Transfer shielded funds to the specified address
-    #[method(name = "shielded_transfer")]
-    async fn shielded_transfer(
-        &self,
-        dest: ShieldedAddress,
-        value_sats: u64,
-        fee_sats: u64,
-    ) -> RpcResult<Txid>;
-
     /// Get total sidechain wealth
     #[method(name = "sidechain_wealth")]
     async fn sidechain_wealth_sats(&self) -> RpcResult<u64>;
 
+    /// Sign a transaction, and optionally broadcast it.
+    #[method(name = "sign_transaction")]
+    async fn sign_transaction(
+        &self,
+        transaction: Transaction,
+        broadcast: Option<bool>,
+    ) -> RpcResult<transaction::Authorized<Transaction>>;
+
+    /// Verify and broadcast a transaction
+    #[method(name = "submit_transaction")]
+    async fn submit_transaction(
+        &self,
+        transaction: transaction::Authorized<Transaction>,
+    ) -> RpcResult<Txid>;
+
     /// Stop the node
     #[method(name = "stop")]
     async fn stop(&self);
-
-    /// Transfer transparent funds to the specified address
-    #[method(name = "transparent_transfer")]
-    async fn transparent_transfer(
-        &self,
-        dest: TransparentAddress,
-        value_sats: u64,
-        fee_sats: u64,
-    ) -> RpcResult<Txid>;
-
-    /// Unshield shielded funds
-    #[method(name = "unshield")]
-    async fn unshield(&self, value_sats: u64, fee_sats: u64)
-    -> RpcResult<Txid>;
-
-    /// Initiate a withdrawal to the specified mainchain address
-    #[method(name = "withdraw")]
-    async fn withdraw(
-        &self,
-        #[open_api_method_arg(schema(
-            PartialSchema = "thunder_orchard::types::schema::BitcoinAddr"
-        ))]
-        mainchain_address: bitcoin::Address<
-            bitcoin::address::NetworkUnchecked,
-        >,
-        amount_sats: u64,
-        fee_sats: u64,
-        mainchain_fee_sats: u64,
-    ) -> RpcResult<Txid>;
 }