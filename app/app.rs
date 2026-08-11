LayerTwo-Labs/thunder-rust

RPC: regenerate utreexo proofs in `submit_transaction`

app/app.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
 
 use fallible_iterator::FallibleIterator as _;
 use futures::{StreamExt, TryFutureExt};
@@ -280,10 +280,11 @@ impl App {
         update(self.node.as_ref(), &mut self.utxos.write(), &self.wallet)
     }
 
-    pub fn submit_transaction(
-        &self,
-        tx: &thunder::types::AuthorizedTransaction,
-    ) -> Result<(), Error> {
+    /// Regenerate proofs and submit transaction
+    pub fn submit_transaction<Tx>(&self, tx: Tx) -> Result<(), Error>
+    where
+        Tx: BorrowMut<thunder::types::AuthorizedTransaction>,
+    {
         self.node.submit_transaction(tx)?;
         let () = self.update()?;
         Ok(())
@@ -291,7 +292,7 @@ impl App {
 
     pub fn sign_and_send(&self, tx: Transaction) -> Result<(), Error> {
         let authorized_transaction = self.wallet.authorize(tx)?;
-        self.submit_transaction(&authorized_transaction)
+        self.submit_transaction(authorized_transaction)
     }
 
     pub async fn get_new_main_address(

app/rpc_server.rs
@@ -332,12 +332,12 @@ impl RpcServer for RpcServerImpl {
         transaction: thunder::types::Transaction,
         broadcast: Option<bool>,
     ) -> RpcResult<thunder::types::AuthorizedTransaction> {
-        let authorized =
+        let mut authorized =
             self.app.wallet.authorize(transaction).map_err(custom_err)?;
         if let Some(true) = broadcast {
             let () = self
                 .app
-                .submit_transaction(&authorized)
+                .submit_transaction(&mut authorized)
                 .map_err(custom_err)?;
         }
         Ok(authorized)
@@ -345,11 +345,11 @@ impl RpcServer for RpcServerImpl {
 
     async fn submit_transaction(
         &self,
-        transaction: thunder::types::AuthorizedTransaction,
+        mut transaction: thunder::types::AuthorizedTransaction,
     ) -> RpcResult<Txid> {
         let () = self
             .app
-            .submit_transaction(&transaction)
+            .submit_transaction(&mut transaction)
             .map_err(custom_err)?;
         Ok(transaction.transaction.txid())
     }

lib/node/mod.rs
@@ -1,4 +1,5 @@
 use std::{
+    borrow::BorrowMut,
     collections::{HashMap, HashSet},
     fmt::Debug,
     net::SocketAddr,
@@ -244,17 +245,26 @@ where
         Ok(self.state.try_get_tip(&rotxn)?)
     }
 
-    pub fn submit_transaction(
+    /// Regenerate proofs and submit transaction
+    pub fn submit_transaction<Tx>(
         &self,
-        transaction: &AuthorizedTransaction,
-    ) -> Result<(), Error> {
+        mut transaction: Tx,
+    ) -> Result<(), Error>
+    where
+        Tx: BorrowMut<AuthorizedTransaction>,
+    {
         {
             let mut rotxn = self.env.write_txn().map_err(EnvError::from)?;
-            self.state.validate_transaction(&rotxn, transaction)?;
-            self.mempool.put(&mut rotxn, transaction)?;
+            self.state.regenerate_proof(
+                &rotxn,
+                &mut transaction.borrow_mut().transaction,
+            )?;
+            self.state
+                .validate_transaction(&rotxn, transaction.borrow())?;
+            self.mempool.put(&mut rotxn, transaction.borrow())?;
             rotxn.commit().map_err(RwTxnError::from)?;
         }
-        self.net.push_tx(Default::default(), transaction);
+        self.net.push_tx(Default::default(), transaction.borrow());
         Ok(())
     }
 

iwakura-rein/thunder-orchard

app/app.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
 
 use fallible_iterator::FallibleIterator as _;
 use futures::{StreamExt, TryFutureExt};
@@ -405,15 +405,16 @@ impl App {
                 orchard_bundle,
             )?;
         }
-        self.node.submit_transaction(&authorized_transaction)?;
+        self.node.submit_transaction(authorized_transaction)?;
         let () = self.update(wallet_rwtxn)?;
         Ok(())
     }
 
-    pub fn submit_transaction(
-        &self,
-        tx: &thunder_orchard::types::AuthorizedTransaction,
-    ) -> Result<(), Error> {
+    /// Regenerate proofs and submit transaction
+    pub fn submit_transaction<Tx>(&self, tx: Tx) -> Result<(), Error>
+    where
+        Tx: BorrowMut<thunder_orchard::types::AuthorizedTransaction>,
+    {
         self.node.submit_transaction(tx)?;
         let wallet_rwtxn =
             self.wallet.env().write_txn().map_err(wallet::Error::from)?;

app/rpc_server.rs
@@ -522,12 +522,12 @@ impl RpcServer for RpcServerImpl {
         transaction: thunder_orchard::types::Transaction,
         broadcast: Option<bool>,
     ) -> RpcResult<thunder_orchard::types::AuthorizedTransaction> {
-        let authorized =
+        let mut authorized =
             self.app.wallet.authorize(transaction).map_err(custom_err)?;
         if let Some(true) = broadcast {
             let () = self
                 .app
-                .submit_transaction(&authorized)
+                .submit_transaction(&mut authorized)
                 .map_err(custom_err)?;
         }
         Ok(authorized)
@@ -535,11 +535,11 @@ impl RpcServer for RpcServerImpl {
 
     async fn submit_transaction(
         &self,
-        transaction: thunder_orchard::types::AuthorizedTransaction,
+        mut transaction: thunder_orchard::types::AuthorizedTransaction,
     ) -> RpcResult<Txid> {
         let () = self
             .app
-            .submit_transaction(&transaction)
+            .submit_transaction(&mut transaction)
             .map_err(custom_err)?;
         Ok(transaction.transaction.txid())
     }

lib/node/mod.rs
@@ -1,5 +1,5 @@
 use std::{
-    borrow::Cow,
+    borrow::{BorrowMut, Cow},
     collections::{HashMap, HashSet},
     net::SocketAddr,
     path::Path,
@@ -188,17 +188,26 @@ where
         Ok(self.state.try_get_tip(&rotxn).map_err(state::Error::from)?)
     }
 
-    pub fn submit_transaction(
+    /// Regenerate proofs and submit transaction
+    pub fn submit_transaction<Tx>(
         &self,
-        transaction: &AuthorizedTransaction,
-    ) -> Result<(), error::SubmitTransaction> {
+        mut transaction: Tx,
+    ) -> Result<(), error::SubmitTransaction>
+    where
+        Tx: BorrowMut<AuthorizedTransaction>,
+    {
         {
             let mut rwtxn = self.env.write_txn()?;
-            self.state.validate_transaction(&rwtxn, transaction)?;
-            self.mempool.insert(&mut rwtxn, transaction)?;
+            let () = self.state.regenerate_proof(
+                &rwtxn,
+                &mut transaction.borrow_mut().transaction,
+            )?;
+            self.state
+                .validate_transaction(&rwtxn, transaction.borrow())?;
+            self.mempool.insert(&mut rwtxn, transaction.borrow())?;
             rwtxn.commit()?;
         }
-        self.net.push_tx(Default::default(), transaction);
+        self.net.push_tx(Default::default(), transaction.borrow());
         Ok(())
     }
 