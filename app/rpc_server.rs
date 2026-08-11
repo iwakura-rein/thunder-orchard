LayerTwo-Labs/thunder-rust

RPC: add get_stxos/get_utxos methods

app/rpc_server.rs
@@ -1,4 +1,4 @@
-use std::net::SocketAddr;
+use std::{collections::HashSet, net::SocketAddr};
 
 use bitcoin::Amount;
 use jsonrpsee::{
@@ -7,7 +7,8 @@ use jsonrpsee::{
     types::ErrorObject,
 };
 use thunder::types::{
-    Address, PointedOutput, Txid, WithdrawalBundle, net::Peer, wallet::Balance,
+    Address, Pointed, PointedOutput, SpentOutput, Txid, WithdrawalBundle,
+    net::Peer, wallet::Balance,
 };
 use thunder_app_rpc_api::{GetTransactionResponse, RpcServer};
 use tower_http::{
@@ -189,6 +190,21 @@ impl RpcServer for RpcServerImpl {
         self.app.wallet.get_new_address().map_err(custom_err)
     }
 
+    async fn get_stxos(
+        &self,
+        addresses: HashSet<Address>,
+    ) -> RpcResult<Vec<Pointed<SpentOutput>>> {
+        let res = self
+            .app
+            .node
+            .get_stxos_by_addresses(&addresses)
+            .map_err(custom_err)?
+            .into_iter()
+            .map(|(outpoint, output)| Pointed { outpoint, output })
+            .collect();
+        Ok(res)
+    }
+
     async fn get_transaction(
         &self,
         txid: Txid,
@@ -199,6 +215,21 @@ impl RpcServer for RpcServerImpl {
             .try_get_transaction(txid)
             .map_err(custom_err)?
             .map(|(tx, block_hash)| GetTransactionResponse { tx, block_hash });
+        Ok(res)
+    }
+
+    async fn get_utxos(
+        &self,
+        addresses: HashSet<Address>,
+    ) -> RpcResult<Vec<PointedOutput>> {
+        let res = self
+            .app
+            .node
+            .get_utxos_by_addresses(&addresses)
+            .map_err(custom_err)?
+            .into_iter()
+            .map(|(outpoint, output)| PointedOutput { outpoint, output })
+            .collect();
         Ok(res)
     }
 

cli/lib.rs
@@ -77,8 +77,18 @@ pub enum Command {
     },
     /// Get a new address
     GetNewAddress,
+    /// Get stxos for addresses
+    GetStxos {
+        #[arg(required = true)]
+        addresses: Vec<Address>,
+    },
     /// Get transaction by txid
     GetTransaction { txid: Txid },
+    /// Get utxos for addresses
+    GetUtxos {
+        #[arg(required = true)]
+        addresses: Vec<Address>,
+    },
     /// Get wallet addresses, sorted by base58 encoding
     GetWalletAddresses,
     /// Get wallet UTXOs
@@ -224,9 +234,19 @@ where
             let address = rpc_client.get_new_address().await?;
             format!("{address}")
         }
+        Command::GetStxos { addresses } => {
+            let addresses = addresses.into_iter().collect();
+            let stxos = rpc_client.get_stxos(addresses).await?;
+            serde_json::to_string_pretty(&stxos)?
+        }
         Command::GetTransaction { txid } => {
             let tx_info = rpc_client.get_transaction(txid).await?;
             serde_json::to_string_pretty(&tx_info)?
+        }
+        Command::GetUtxos { addresses } => {
+            let addresses = addresses.into_iter().collect();
+            let utxos = rpc_client.get_utxos(addresses).await?;
+            serde_json::to_string_pretty(&utxos)?
         }
         Command::GetWalletAddresses => {
             let addresses = rpc_client.get_wallet_addresses().await?;

lib/node/mod.rs
@@ -297,6 +297,18 @@ where
         Ok(spent)
     }
 
+    pub fn get_stxos_by_addresses(
+        &self,
+        addresses: &HashSet<Address>,
+    ) -> Result<HashMap<OutPoint, SpentOutput>, Error> {
+        let rotxn = self.env.read_txn()?;
+        let stxos = self
+            .state
+            .get_stxos_by_addresses(&rotxn, addresses)
+            .map_err(DbError::from)?;
+        Ok(stxos)
+    }
+
     pub fn get_utxos_by_addresses(
         &self,
         addresses: &HashSet<Address>,

lib/state/mod.rs
@@ -175,6 +175,25 @@ impl State {
         Ok(height)
     }
 
+    pub fn get_stxos_by_addresses(
+        &self,
+        rotxn: &RoTxn,
+        addresses: &HashSet<Address>,
+    ) -> Result<HashMap<OutPoint, SpentOutput>, db_error::Iter> {
+        let stxos: HashMap<OutPoint, _> = self
+            .stxos
+            .iter(rotxn)?
+            .filter_map(|(key, output)| {
+                if addresses.contains(&output.output.address) {
+                    Ok(Some((key.into(), output)))
+                } else {
+                    Ok(None)
+                }
+            })
+            .collect()?;
+        Ok(stxos)
+    }
+
     pub fn get_utxos(
         &self,
         rotxn: &RoTxn,

iwakura-rein/thunder-orchard

app/rpc_server.rs
@@ -1,4 +1,4 @@
-use std::net::SocketAddr;
+use std::{collections::HashSet, net::SocketAddr};
 
 use bitcoin::Amount;
 use jsonrpsee::{
@@ -7,8 +7,8 @@ use jsonrpsee::{
     types::ErrorObject,
 };
 use thunder_orchard::types::{
-    PointedOutput, ShieldedAddress, SpentOutput, TransparentAddress, Txid,
-    WithdrawalBundle, net::Peer, wallet::Balance,
+    Pointed, PointedOutput, ShieldedAddress, SpentOutput, TransparentAddress,
+    Txid, WithdrawalBundle, net::Peer, wallet::Balance,
 };
 use thunder_orchard_app_rpc_api::{GetTransactionResponse, RpcServer};
 use tower_http::{
@@ -298,6 +298,21 @@ impl RpcServer for RpcServerImpl {
         Ok(res)
     }
 
+    async fn get_stxos(
+        &self,
+        addresses: HashSet<TransparentAddress>,
+    ) -> RpcResult<Vec<Pointed<SpentOutput>>> {
+        let res = self
+            .app
+            .node
+            .get_stxos_by_addresses(&addresses)
+            .map_err(custom_err)?
+            .into_iter()
+            .map(|(outpoint, output)| Pointed { outpoint, output })
+            .collect();
+        Ok(res)
+    }
+
     async fn get_transaction(
         &self,
         txid: Txid,
@@ -325,6 +340,21 @@ impl RpcServer for RpcServerImpl {
         };
         let mut res: Vec<_> = addrs.into_iter().collect();
         res.sort_by_key(|addr| addr.as_base58());
+        Ok(res)
+    }
+
+    async fn get_utxos(
+        &self,
+        addresses: HashSet<TransparentAddress>,
+    ) -> RpcResult<Vec<PointedOutput>> {
+        let res = self
+            .app
+            .node
+            .get_utxos_by_addresses(&addresses)
+            .map_err(custom_err)?
+            .into_iter()
+            .map(|(outpoint, output)| PointedOutput { outpoint, output })
+            .collect();
         Ok(res)
     }
 

cli/lib.rs
@@ -104,10 +104,20 @@ pub enum Command {
     GetNewTransparentAddress,
     /// Get shielded wallet addresses, sorted by bech32m encoding
     GetShieldedWalletAddresses,
+    /// Get stxos for addresses
+    GetStxos {
+        #[arg(required = true)]
+        addresses: Vec<TransparentAddress>,
+    },
     /// Get transaction by txid
     GetTransaction { txid: Txid },
     /// Get transparent wallet addresses, sorted by base58 encoding
     GetTransparentWalletAddresses,
+    /// Get utxos for addresses
+    GetUtxos {
+        #[arg(required = true)]
+        addresses: Vec<TransparentAddress>,
+    },
     /// Get wallet STXOs
     GetWalletStxos,
     /// Get unconfirmed wallet STXOs
@@ -297,6 +307,11 @@ where
             let addresses = rpc_client.get_shielded_wallet_addresses().await?;
             serde_json::to_string_pretty(&addresses)?
         }
+        Command::GetStxos { addresses } => {
+            let addresses = addresses.into_iter().collect();
+            let stxos = rpc_client.get_stxos(addresses).await?;
+            serde_json::to_string_pretty(&stxos)?
+        }
         Command::GetTransaction { txid } => {
             let tx_info = rpc_client.get_transaction(txid).await?;
             serde_json::to_string_pretty(&tx_info)?
@@ -305,6 +320,11 @@ where
             let addresses =
                 rpc_client.get_transparent_wallet_addresses().await?;
             serde_json::to_string_pretty(&addresses)?
+        }
+        Command::GetUtxos { addresses } => {
+            let addresses = addresses.into_iter().collect();
+            let utxos = rpc_client.get_utxos(addresses).await?;
+            serde_json::to_string_pretty(&utxos)?
         }
         Command::GetWalletStxos => {
             let stxos = rpc_client.get_wallet_stxos().await?;

lib/node/mod.rs
@@ -241,6 +241,18 @@ where
         Ok(spent)
     }
 
+    pub fn get_stxos_by_addresses(
+        &self,
+        addresses: &HashSet<TransparentAddress>,
+    ) -> Result<HashMap<OutPoint, SpentOutput>, Error> {
+        let rotxn = self.env.read_txn()?;
+        let stxos = self
+            .state
+            .get_stxos_by_addresses(&rotxn, addresses)
+            .map_err(DbError::from)?;
+        Ok(stxos)
+    }
+
     pub fn get_utxos_by_addresses(
         &self,
         addresses: &HashSet<TransparentAddress>,

lib/state/mod.rs
@@ -175,6 +175,25 @@ impl State {
         Ok(height)
     }
 
+    pub fn get_stxos_by_addresses(
+        &self,
+        rotxn: &RoTxn,
+        addresses: &HashSet<TransparentAddress>,
+    ) -> Result<HashMap<OutPoint, SpentOutput>, db_error::Iter> {
+        let stxos: HashMap<OutPoint, _> = self
+            .stxos
+            .iter(rotxn)?
+            .filter_map(|(key, output)| {
+                if addresses.contains(&output.output.address) {
+                    Ok(Some((key.into(), output)))
+                } else {
+                    Ok(None)
+                }
+            })
+            .collect()?;
+        Ok(stxos)
+    }
+
     pub fn get_utxos(
         &self,
         rotxn: &RoTxn,