LayerTwo-Labs/thunder-rust

RPC: add invalidate_block

app/rpc_server.rs
@@ -87,6 +87,16 @@ impl rpc_api::node::PrivateRpcServer for RpcServerImpl<true> {
         }
     }
 
+    async fn invalidate_block(
+        &self,
+        block_hash: thunder::types::BlockHash,
+    ) -> RpcResult<()> {
+        self.app
+            .node
+            .invalidate_block(block_hash)
+            .map_err(custom_err)
+    }
+
     async fn remove_from_mempool(&self, txid: Txid) -> RpcResult<()> {
         self.app.node.remove_from_mempool(txid).map_err(custom_err)
     }

cli/lib.rs
@@ -106,6 +106,11 @@ pub enum Command {
     GetWalletUtxos,
     /// Get the current block count
     GetBlockcount,
+    /// Invalidate a block, potentially re-orging to a valid ancestor of the
+    /// current tip.
+    InvalidateBlock {
+        block_hash: thunder::types::BlockHash,
+    },
     /// Get the height of the latest failed withdrawal bundle
     LatestFailedWithdrawalBundleHeight,
     /// List peers
@@ -283,6 +288,10 @@ where
         Command::GetBlockcount => {
             let blockcount = rpc_client.getblockcount().await?;
             format!("{blockcount}")
+        }
+        Command::InvalidateBlock { block_hash } => {
+            let () = rpc_client.invalidate_block(block_hash).await?;
+            String::default()
         }
         Command::LatestFailedWithdrawalBundleHeight => {
             let height =

iwakura-rein/thunder-orchard

app/rpc_server.rs
@@ -87,6 +87,16 @@ impl rpc_api::node::PrivateRpcServer for RpcServerImpl<true> {
         }
     }
 
+    async fn invalidate_block(
+        &self,
+        block_hash: thunder_orchard::types::BlockHash,
+    ) -> RpcResult<()> {
+        self.app
+            .node
+            .invalidate_block(block_hash)
+            .map_err(custom_err)
+    }
+
     async fn remove_from_mempool(&self, txid: Txid) -> RpcResult<()> {
         self.app.node.remove_from_mempool(txid).map_err(custom_err)
     }

cli/lib.rs
@@ -139,6 +139,11 @@ pub enum Command {
     GetWalletUtxosUnconfirmed,
     /// Get the current block count
     GetBlockcount,
+    /// Invalidate a block, potentially re-orging to a valid ancestor of the
+    /// current tip.
+    InvalidateBlock {
+        block_hash: thunder_orchard::types::BlockHash,
+    },
     /// Get the height of the latest failed withdrawal bundle
     LatestFailedWithdrawalBundleHeight,
     /// List peers
@@ -369,6 +374,10 @@ where
         Command::GetBlockcount => {
             let blockcount = rpc_client.getblockcount().await?;
             format!("{blockcount}")
+        }
+        Command::InvalidateBlock { block_hash } => {
+            let () = rpc_client.invalidate_block(block_hash).await?;
+            String::default()
         }
         Command::LatestFailedWithdrawalBundleHeight => {
             let height =