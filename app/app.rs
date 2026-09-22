LayerTwo-Labs/thunder-rust

net: specify server names correctly

app/app.rs
@@ -1,4 +1,10 @@
-use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
+use std::{
+    borrow::BorrowMut,
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+    path::PathBuf,
+    sync::Arc,
+};
 
 use fallible_iterator::FallibleIterator as _;
 use futures::{StreamExt, TryFutureExt};
@@ -25,8 +31,6 @@ use tonic_health::{
     ServingStatus,
     pb::{HealthCheckRequest, health_client::HealthClient},
 };
-
-use crate::cli::Config;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -102,6 +106,18 @@ pub struct BlockTemplate {
     pub body: types::Body,
     /// Fees collected by the transactions in the block
     pub fees: bitcoin::Amount,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub add_peers: HashSet<thunder::types::net::PeerAddress>,
+    pub datadir: PathBuf,
+    pub mainchain_grpc_url: url::Url,
+    pub mnemonic_seed_phrase_path: Option<PathBuf>,
+    pub net_addr: SocketAddr,
+    pub network: thunder::types::Network,
+    pub network_magic_override: Option<thunder::net::peer_message::MagicBytes>,
+    pub server_names: HashSet<String>,
 }
 
 #[derive(Clone)]
@@ -214,7 +230,7 @@ impl App {
         Ok(res)
     }
 
-    pub fn new(config: &Config) -> Result<Self, Error> {
+    pub fn new(config: Config) -> Result<Self, Error> {
         // Node launches some tokio tasks for p2p networking, that is why we need a tokio runtime
         // here.
         let runtime = tokio::runtime::Builder::new_multi_thread()
@@ -278,11 +294,12 @@ impl App {
         tracing::debug!("Instantiating node struct");
         let node = Node::new(
             thunder::node::Config {
-                datadir: &config.datadir,
+                add_peers: config.add_peers,
                 bind_addr: config.net_addr,
+                datadir: config.datadir,
                 magic_bytes_override: config.network_magic_override,
-                peers: &config.peers,
                 network: config.network,
+                server_names: config.server_names,
             },
             cusf_mainchain,
             cusf_mainchain_wallet,

app/cli.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     net::{IpAddr, Ipv4Addr, SocketAddr},
     ops::Deref,
     path::PathBuf,
@@ -103,6 +104,11 @@ fn parse_network_magic(s: &str) -> Result<[u8; 4], const_hex::FromHexError> {
 #[derive(Clone, Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub(super) struct Cli {
+    /// Additional peers to dial on startup, as `host:port`. May be given
+    /// more than once, and is dialed in addition to the network's built-in
+    /// seed peers.
+    #[arg(long = "add-peer")]
+    add_peers: Vec<thunder::types::net::PeerAddress>,
     /// Data directory for storing blockchain and wallet data
     #[command(flatten)]
     datadir: DatadirArg,
@@ -141,21 +147,21 @@ pub(super) struct Cli {
     /// Manually provide the network magic bytes
     #[arg(long, value_parser = parse_network_magic)]
     network_magic: Option<[u8; 4]>,
-    /// Additional peers to dial on startup, as `host:port`. May be given
-    /// more than once, and is dialed in addition to the network's built-in
-    /// seed peers.
-    #[arg(long = "add-peer")]
-    peers: Vec<thunder::types::net::PeerAddress>,
     /// Socket address to host the private RPC server
     #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
     private_rpc_addr: SocketAddr,
     /// Socket address to host the RPC server
     #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
     rpc_addr: SocketAddr,
+    /// Host name used by the p2p server.
+    /// This option can be specified multiple times.
+    #[arg(long = "server-name")]
+    server_names: Vec<String>,
 }
 
 #[derive(Clone, Debug)]
 pub struct Config {
+    pub add_peers: HashSet<thunder::types::net::PeerAddress>,
     pub datadir: PathBuf,
     pub headless: bool,
     /// If None, logging to file should be disabled.
@@ -167,9 +173,9 @@ pub struct Config {
     pub net_addr: SocketAddr,
     pub network: Network,
     pub network_magic_override: Option<thunder::net::peer_message::MagicBytes>,
-    pub peers: Vec<thunder::types::net::PeerAddress>,
     pub private_rpc_addr: SocketAddr,
     pub rpc_addr: SocketAddr,
+    pub server_names: HashSet<String>,
 }
 
 impl Cli {
@@ -196,6 +202,7 @@ impl Cli {
             saturating_pred_level(self.log_level)
         };
         Ok(Config {
+            add_peers: HashSet::from_iter(self.add_peers),
             datadir: self.datadir.0,
             headless: self.headless,
             log_dir,
@@ -206,9 +213,9 @@ impl Cli {
             net_addr: self.net_addr,
             network: self.network,
             network_magic_override: self.network_magic,
-            peers: self.peers,
             private_rpc_addr: self.private_rpc_addr,
             rpc_addr: self.rpc_addr,
+            server_names: HashSet::from_iter(self.server_names),
         })
     }
 }

iwakura-rein/thunder-orchard

app/app.rs
@@ -1,7 +1,14 @@
-use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
+use std::{
+    borrow::BorrowMut,
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+    path::PathBuf,
+    sync::Arc,
+};
 
 use fallible_iterator::FallibleIterator as _;
 use futures::{StreamExt, TryFutureExt};
+use thiserror::Error;
 use thunder_orchard::{
     miner::{self, Miner},
     node::{self, Node},
@@ -24,11 +31,10 @@ use tonic_health::{
     ServingStatus,
     pb::{HealthCheckRequest, health_client::HealthClient},
 };
-
-use crate::cli::Config;
+use transitive::Transitive;
 
 #[allow(clippy::duplicated_attributes)]
-#[derive(Debug, thiserror::Error, transitive::Transitive)]
+#[derive(Debug, Error, Transitive)]
 #[transitive(
     from(thunder_orchard::archive::Error, node::Error),
     from(thunder_orchard::state::Error, node::Error)
@@ -169,6 +175,19 @@ pub struct BlockTemplate {
     pub fees: bitcoin::Amount,
 }
 
+#[derive(Debug)]
+pub struct Config {
+    pub add_peers: HashSet<thunder_orchard::types::net::PeerAddress>,
+    pub datadir: PathBuf,
+    pub mainchain_grpc_url: url::Url,
+    pub mnemonic_seed_phrase_path: Option<PathBuf>,
+    pub net_addr: SocketAddr,
+    pub network: thunder_orchard::types::Network,
+    pub network_magic_override:
+        Option<thunder_orchard::net::peer_message::MagicBytes>,
+    pub server_names: HashSet<String>,
+}
+
 #[derive(Clone)]
 pub struct App {
     pub node: Arc<Node>,
@@ -271,7 +290,7 @@ impl App {
         Ok(res)
     }
 
-    pub fn new(config: &Config) -> Result<Self, Error> {
+    pub fn new(config: Config) -> Result<Self, Error> {
         // Node launches some tokio tasks for p2p networking, that is why we need a tokio runtime
         // here.
         let runtime = tokio::runtime::Builder::new_multi_thread()
@@ -332,11 +351,12 @@ impl App {
         tracing::debug!("Instantiating node struct");
         let node = Node::new(
             thunder_orchard::node::Config {
-                datadir: &config.datadir,
+                add_peers: config.add_peers,
                 bind_addr: config.net_addr,
+                datadir: config.datadir,
                 magic_bytes_override: config.network_magic_override,
-                peers: &config.peers,
                 network: config.network,
+                server_names: config.server_names,
             },
             cusf_mainchain,
             cusf_mainchain_wallet,

app/cli.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     net::{IpAddr, Ipv4Addr, SocketAddr},
     ops::Deref,
     path::PathBuf,
@@ -103,6 +104,11 @@ fn parse_network_magic(s: &str) -> Result<[u8; 4], const_hex::FromHexError> {
 #[derive(Clone, Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub(super) struct Cli {
+    /// Additional peers to dial on startup, as `host:port`. May be given
+    /// more than once, and is dialed in addition to the network's built-in
+    /// seed peers.
+    #[arg(long = "add-peer")]
+    add_peers: Vec<thunder_orchard::types::net::PeerAddress>,
     /// Data directory for storing blockchain and wallet data
     #[command(flatten)]
     datadir: DatadirArg,
@@ -142,21 +148,21 @@ pub(super) struct Cli {
     /// Manually provide the network magic bytes
     #[arg(long, value_parser = parse_network_magic)]
     network_magic: Option<[u8; 4]>,
-    /// Additional peers to dial on startup, as `host:port`. May be given
-    /// more than once, and is dialed in addition to the network's built-in
-    /// seed peers.
-    #[arg(long = "add-peer")]
-    peers: Vec<thunder_orchard::types::net::PeerAddress>,
     /// Socket address to host the private RPC server
     #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
     private_rpc_addr: SocketAddr,
     /// Socket address to host the RPC server
     #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
     rpc_addr: SocketAddr,
+    /// Host name used by the p2p server.
+    /// This option can be specified multiple times.
+    #[arg(long = "server-name")]
+    server_names: Vec<String>,
 }
 
 #[derive(Clone, Debug)]
 pub struct Config {
+    pub add_peers: HashSet<thunder_orchard::types::net::PeerAddress>,
     pub datadir: PathBuf,
     pub headless: bool,
     /// If None, logging to file should be disabled.
@@ -169,9 +175,9 @@ pub struct Config {
     pub network: Network,
     pub network_magic_override:
         Option<thunder_orchard::net::peer_message::MagicBytes>,
-    pub peers: Vec<thunder_orchard::types::net::PeerAddress>,
     pub private_rpc_addr: SocketAddr,
     pub rpc_addr: SocketAddr,
+    pub server_names: HashSet<String>,
 }
 
 impl Cli {
@@ -198,6 +204,7 @@ impl Cli {
             saturating_pred_level(self.log_level)
         };
         Ok(Config {
+            add_peers: HashSet::from_iter(self.add_peers),
             datadir: self.datadir.0,
             headless: self.headless,
             log_dir,
@@ -208,9 +215,9 @@ impl Cli {
             net_addr: self.net_addr,
             network: self.network,
             network_magic_override: self.network_magic,
-            peers: self.peers,
             private_rpc_addr: self.private_rpc_addr,
             rpc_addr: self.rpc_addr,
+            server_names: HashSet::from_iter(self.server_names),
         })
     }
 }