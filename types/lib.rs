LayerTwo-Labs/thunder-rust

Types: move types/transaction.rs to types/transaction/*

types/lib.rs
@@ -35,9 +35,9 @@ pub mod schema;
 pub mod state;
 pub mod transaction;
 pub use transaction::{
-    Authorized, AuthorizedTransaction, Content as OutputContent,
-    FilledTransaction, GetAddress, GetValue, InPoint, OutPoint, OutPointKey,
-    Output, Pointed, PointedOutput, PointedOutputRef, SpentOutput, Transaction,
+    Authorized, AuthorizedTransaction, FilledTransaction, GetAddress, GetValue,
+    InPoint, OutPoint, OutPointKey, Output, OutputContent, PointedOutput,
+    PointedOutputRef, SpentOutput, Transaction,
 };
 mod util;
 pub mod wallet;

types/transaction/output/mod.rs
@@ -1,0 +1,69 @@
+use borsh::BorshSerialize;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+use crate::{
+    address::Address,
+    hashes::{UtreexoNodeHash, hash},
+    transaction::{GetValue, outpoint::OutPoint},
+};
+
+mod content;
+pub use content::Content;
+
+#[derive(
+    BorshSerialize,
+    Clone,
+    Debug,
+    Deserialize,
+    Eq,
+    PartialEq,
+    Serialize,
+    ToSchema,
+)]
+pub struct Output {
+    pub address: Address,
+    pub content: Content,
+}
+
+impl GetValue for Output {
+    #[inline(always)]
+    fn get_value(&self) -> bitcoin::Amount {
+        self.content.get_value()
+    }
+}
+
+#[derive(
+    BorshSerialize,
+    Clone,
+    Debug,
+    Deserialize,
+    Eq,
+    PartialEq,
+    Serialize,
+    ToSchema,
+)]
+pub struct Pointed<Output = crate::transaction::output::Output> {
+    pub outpoint: OutPoint,
+    pub output: Output,
+}
+
+impl From<&Pointed> for UtreexoNodeHash {
+    fn from(pointed_output: &Pointed) -> Self {
+        Self::new(hash(pointed_output))
+    }
+}
+
+/// Useful when computing hashes for Utreexo,
+/// without needing to clone an output
+#[derive(BorshSerialize, Clone, Copy, Debug)]
+pub struct PointedOutputRef<'a> {
+    pub outpoint: OutPoint,
+    pub output: &'a Output,
+}
+
+impl From<PointedOutputRef<'_>> for UtreexoNodeHash {
+    fn from(pointed_output: PointedOutputRef) -> Self {
+        Self::new(hash(&pointed_output))
+    }
+}

iwakura-rein/thunder-orchard

types/lib.rs
@@ -34,8 +34,8 @@ pub mod schema;
 pub mod state;
 pub mod transaction;
 pub use transaction::{
-    AuthorizedTransaction, Content as OutputContent, FilledTransaction,
-    GetValue, InPoint, OutPoint, OutPointKey, Output, Pointed, PointedOutput,
+    Authorized, AuthorizedTransaction, FilledTransaction, GetValue, InPoint,
+    OutPoint, OutPointKey, Output, OutputContent, PointedOutput,
     PointedOutputRef, SpentOutput, Transaction,
 };
 mod util;

types/transaction/output/mod.rs
@@ -1,0 +1,69 @@
+use borsh::BorshSerialize;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+use crate::{
+    address::TransparentAddress,
+    hashes::{UtreexoNodeHash, hash},
+    transaction::{GetValue, outpoint::OutPoint},
+};
+
+mod content;
+pub use content::Content;
+
+#[derive(
+    BorshSerialize,
+    Clone,
+    Debug,
+    Deserialize,
+    Eq,
+    PartialEq,
+    Serialize,
+    ToSchema,
+)]
+pub struct Output {
+    pub address: TransparentAddress,
+    pub content: Content,
+}
+
+impl GetValue for Output {
+    #[inline(always)]
+    fn get_value(&self) -> bitcoin::Amount {
+        self.content.get_value()
+    }
+}
+
+#[derive(
+    BorshSerialize,
+    Clone,
+    Debug,
+    Deserialize,
+    Eq,
+    PartialEq,
+    Serialize,
+    ToSchema,
+)]
+pub struct Pointed<Output = crate::transaction::output::Output> {
+    pub outpoint: OutPoint,
+    pub output: Output,
+}
+
+impl From<&Pointed> for UtreexoNodeHash {
+    fn from(pointed_output: &Pointed) -> Self {
+        Self::new(hash(pointed_output))
+    }
+}
+
+/// Useful when computing hashes for Utreexo,
+/// without needing to clone an output
+#[derive(BorshSerialize, Clone, Copy, Debug)]
+pub struct PointedOutputRef<'a> {
+    pub outpoint: OutPoint,
+    pub output: &'a Output,
+}
+
+impl From<PointedOutputRef<'_>> for UtreexoNodeHash {
+    fn from(pointed_output: PointedOutputRef) -> Self {
+        Self::new(hash(&pointed_output))
+    }
+}