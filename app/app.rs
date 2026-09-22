LayerTwo-Labs/thunder-rust

net: resolve seed names by host name, add as CLI opt

app/app.rs
@@ -277,12 +277,15 @@ impl App {
 
         tracing::debug!("Instantiating node struct");
         let node = Node::new(
-            &config.datadir,
-            config.net_addr,
+            thunder::node::Config {
+                datadir: &config.datadir,
+                bind_addr: config.net_addr,
+                magic_bytes_override: config.network_magic_override,
+                peers: &config.peers,
+                network: config.network,
+            },
             cusf_mainchain,
             cusf_mainchain_wallet,
-            config.network_magic_override,
-            config.network,
             &runtime,
         )?;
         let utxos = {

app/cli.rs
@@ -141,6 +141,11 @@ pub(super) struct Cli {
     /// Manually provide the network magic bytes
     #[arg(long, value_parser = parse_network_magic)]
     network_magic: Option<[u8; 4]>,
+    /// Additional peers to dial on startup, as `host:port`. May be given
+    /// more than once, and is dialed in addition to the network's built-in
+    /// seed peers.
+    #[arg(long = "add-peer")]
+    peers: Vec<thunder::types::net::PeerAddress>,
     /// Socket address to host the private RPC server
     #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
     private_rpc_addr: SocketAddr,
@@ -162,6 +167,7 @@ pub struct Config {
     pub net_addr: SocketAddr,
     pub network: Network,
     pub network_magic_override: Option<thunder::net::peer_message::MagicBytes>,
+    pub peers: Vec<thunder::types::net::PeerAddress>,
     pub private_rpc_addr: SocketAddr,
     pub rpc_addr: SocketAddr,
 }
@@ -200,6 +206,7 @@ impl Cli {
             net_addr: self.net_addr,
             network: self.network,
             network_magic_override: self.network_magic,
+            peers: self.peers,
             private_rpc_addr: self.private_rpc_addr,
             rpc_addr: self.rpc_addr,
         })

app/rpc_server.rs
@@ -8,7 +8,9 @@ use jsonrpsee::{
 };
 use thunder::types::{
     Address, Block, Pointed, PointedOutput, SpentOutput, Txid,
-    WithdrawalBundle, net::Peer, wallet::Balance,
+    WithdrawalBundle,
+    net::{Peer, PeerAddress},
+    wallet::Balance,
 };
 use thunder_app_rpc_api as rpc_api;
 use tower_http::{
@@ -76,11 +78,17 @@ impl rpc_api::open_api::RpcServer for RpcServerImpl<true> {
 
 #[async_trait]
 impl rpc_api::node::PrivateRpcServer for RpcServerImpl<true> {
-    async fn connect_peer(&self, addr: SocketAddr) -> RpcResult<()> {
-        self.app.node.connect_peer(addr).map_err(custom_err)
+    async fn connect_peer(&self, addr: PeerAddress) -> RpcResult<()> {
+        let resolved_addr = thunder::net::resolve_peer_address(addr)
+            .await
+            .map_err(custom_err)?;
+        self.app
+            .node
+            .connect_peer(resolved_addr)
+            .map_err(custom_err)
     }
 
-    async fn forget_peer(&self, addr: SocketAddr) -> RpcResult<()> {
+    async fn forget_peer(&self, addr: PeerAddress) -> RpcResult<()> {
         match self.app.node.forget_peer(&addr) {
             Ok(_) => Ok(()),
             Err(err) => Err(custom_err(err)),

iwakura-rein/thunder-orchard

app/app.rs
@@ -331,12 +331,15 @@ impl App {
 
         tracing::debug!("Instantiating node struct");
         let node = Node::new(
-            &config.datadir,
-            config.net_addr,
+            thunder_orchard::node::Config {
+                datadir: &config.datadir,
+                bind_addr: config.net_addr,
+                magic_bytes_override: config.network_magic_override,
+                peers: &config.peers,
+                network: config.network,
+            },
             cusf_mainchain,
             cusf_mainchain_wallet,
-            config.network_magic_override,
-            config.network,
             &runtime,
         )?;
         let node = Arc::new(node);

app/cli.rs
@@ -142,6 +142,11 @@ pub(super) struct Cli {
     /// Manually provide the network magic bytes
     #[arg(long, value_parser = parse_network_magic)]
     network_magic: Option<[u8; 4]>,
+    /// Additional peers to dial on startup, as `host:port`. May be given
+    /// more than once, and is dialed in addition to the network's built-in
+    /// seed peers.
+    #[arg(long = "add-peer")]
+    peers: Vec<thunder_orchard::types::net::PeerAddress>,
     /// Socket address to host the private RPC server
     #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
     private_rpc_addr: SocketAddr,
@@ -164,6 +169,7 @@ pub struct Config {
     pub network: Network,
     pub network_magic_override:
         Option<thunder_orchard::net::peer_message::MagicBytes>,
+    pub peers: Vec<thunder_orchard::types::net::PeerAddress>,
     pub private_rpc_addr: SocketAddr,
     pub rpc_addr: SocketAddr,
 }
@@ -202,6 +208,7 @@ impl Cli {
             net_addr: self.net_addr,
             network: self.network,
             network_magic_override: self.network_magic,
+            peers: self.peers,
             private_rpc_addr: self.private_rpc_addr,
             rpc_addr: self.rpc_addr,
         })

app/rpc_server.rs
@@ -8,7 +8,9 @@ use jsonrpsee::{
 };
 use thunder_orchard::types::{
     Block, Pointed, PointedOutput, ShieldedAddress, SpentOutput,
-    TransparentAddress, Txid, WithdrawalBundle, net::Peer, wallet::Balance,
+    TransparentAddress, Txid, WithdrawalBundle,
+    net::{Peer, PeerAddress},
+    wallet::Balance,
 };
 use thunder_orchard_app_rpc_api as rpc_api;
 use tower_http::{
@@ -76,11 +78,17 @@ impl rpc_api::open_api::RpcServer for RpcServerImpl<true> {
 
 #[async_trait]
 impl rpc_api::node::PrivateRpcServer for RpcServerImpl<true> {
-    async fn connect_peer(&self, addr: SocketAddr) -> RpcResult<()> {
-        self.app.node.connect_peer(addr).map_err(custom_err)
+    async fn connect_peer(&self, addr: PeerAddress) -> RpcResult<()> {
+        let resolved_addr = thunder_orchard::net::resolve_peer_address(addr)
+            .await
+            .map_err(custom_err)?;
+        self.app
+            .node
+            .connect_peer(resolved_addr)
+            .map_err(custom_err)
     }
 
-    async fn forget_peer(&self, addr: SocketAddr) -> RpcResult<()> {
+    async fn forget_peer(&self, addr: PeerAddress) -> RpcResult<()> {
         match self.app.node.forget_peer(&addr) {
             Ok(_) => Ok(()),
             Err(err) => Err(custom_err(err)),