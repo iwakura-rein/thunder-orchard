LayerTwo-Labs/thunder-rust

RPC: add get_block_hash

app/rpc_server.rs
@@ -157,6 +157,13 @@ impl<const ENABLE_PRIVATE_API: bool> rpc_api::node::RpcServer
         Ok(Some(block))
     }
 
+    async fn get_block_hash(
+        &self,
+        height: u32,
+    ) -> RpcResult<Option<thunder::types::BlockHash>> {
+        self.app.node.try_get_block_hash(height).map_err(custom_err)
+    }
+
     async fn get_best_sidechain_block_hash(
         &self,
     ) -> RpcResult<Option<thunder::types::BlockHash>> {

rpc-api/lib.rs
@@ -101,6 +101,17 @@ pub mod node {
             block_hash: thunder_types::BlockHash,
         ) -> RpcResult<Option<thunder_types::Block>>;
 
+        /// Get the block hash at the specified height in the active chain,
+        /// if it exists
+        #[open_api_method(output_schema(
+            PartialSchema = "schema::Optional<thunder_types::BlockHash>"
+        ))]
+        #[method(name = "get_block_hash")]
+        async fn get_block_hash(
+            &self,
+            height: u32,
+        ) -> RpcResult<Option<thunder_types::BlockHash>>;
+
         /// Get mainchain blocks that commit to a specified block hash
         #[open_api_method(output_schema(
             PartialSchema = "schema::BitcoinBlockHash"

iwakura-rein/thunder-orchard

app/rpc_server.rs
@@ -185,6 +185,13 @@ impl<const ENABLE_PRIVATE_API: bool> rpc_api::node::RpcServer
         Ok(Some(block))
     }
 
+    async fn get_block_hash(
+        &self,
+        height: u32,
+    ) -> RpcResult<Option<thunder_orchard::types::BlockHash>> {
+        self.app.node.try_get_block_hash(height).map_err(custom_err)
+    }
+
     async fn get_bmm_inclusions(
         &self,
         block_hash: thunder_orchard::types::BlockHash,

rpc-api/lib.rs
@@ -121,6 +121,17 @@ pub mod node {
             block_hash: thunder_orchard_types::BlockHash,
         ) -> RpcResult<Option<thunder_orchard_types::Block>>;
 
+        /// Get the block hash at the specified height in the active chain,
+        /// if it exists
+        #[open_api_method(output_schema(
+            PartialSchema = "schema::Optional<thunder_orchard_types::BlockHash>"
+        ))]
+        #[method(name = "get_block_hash")]
+        async fn get_block_hash(
+            &self,
+            height: u32,
+        ) -> RpcResult<Option<thunder_orchard_types::BlockHash>>;
+
         /// Get mainchain blocks that commit to a specified block hash
         #[open_api_method(output_schema(
             PartialSchema = "thunder_orchard_schema::BitcoinBlockHash"